7739343710e62a2a7118fa4ee8cb9bbc49aae4c0
Jørgen Jacobsen <jorgeja@gmail.com>
2021-03-26 12:59:36 +0100

    tomshosting adventure

---
+++ adventure.js
@@ -0,0 +1,332 @@
+var kontoret = [
+  {
+    navn: "Inngangen",
+    beskrivelse:
+      "Du ruller inn i gangen på Toms privathus på Grønli i Porsgrunn. Selv om du er en robot blir du nesten slått i bakken av lukten av sigg og gammel kaffe. På veggen henger bilder av Toms familie: Tommy, Turid og bikkja Horg. På knaggene i gangen henger 10-20 allværsjakker, steinvaskede levis 501, og et bredt utvalg mokkasiner med dusk.",
+  },
+  {
+    navn: "Tommys rom",
+    beskrivelse:
+      "Du bruker robotarmen til å løsne dørhåndtaket på døra til Tommys rom, og ruller inn. Rommet bærer preg av å ikke ha blitt brukt på en stund, og er derfor psykopatisk ryddig.",
+    steder: [
+      {
+        navn: "Dataen til Tommy",
+        beskrivelse:
+          "Battlestation til Tommy, full av klistremerker av klassiske humorperler som Tommy fra Tommy og tigeren som pisser på en Apple-logo. Selv om du bare er en robot, så må du le litt. Du skrur på maskinen, men den ser ut til å trenge en kode.",
+        bruk: {
+          turidkode: {
+            beskrivelse:
+              "Du taster inn koden du fant på bildet til Turid, og plutselig er du logget inn på Turids konto på maskinen til Tommy. WTF? Det ser jaggu ut til at flere bruker maskinen til Tommy. Teksten 'Velkommen Turidshosting, Porsgrunns fjerde beste hostingtjeneste.' blinker mot deg. Under står teksten, 'dagens kode er 1337'",
+            gjenstand: "dagenskode",
+          },
+          tommykode: {
+            beskrivelse:
+              "Du taster inn koden og logger inn på Tommys maskin. Selv om du leter blir du skuffet. Det ser ut til at Tommy ikke har gjort stort annet enn å spille Rocket League i det siste.",
+          },
+        },
+      },
+      {
+        navn: "senga til Tommy",
+        beskrivelse:
+          "Du inspiserer senga til Tommy, og finner ikke stort annet enn en Coca-cola-kork fra en glasscola hvor det står 'I dag blir dagen du blir kjendis', og en litt ødelagt minnepenn fra Kjel og Company på Down Town.",
+        gjenstand: "minnepenn",
+      },
+      {
+        navn: "Klesskapet til Tommy",
+        beskrivelse:
+          "Du tar en titt i klesskapet til Tommy og opppdager en fargekoordinert samling hettegensere og khaki-bukser. På innsiden av skapet henger det et bilde av Mark Zuckerberg, i trusa.",
+        gjenstand: "bildeavmark",
+      },
+    ],
+  },
+  {
+    navn: "Turids rom",
+    beskrivelse:
+      "Døren til Turid - mora til Tom - sitt gjesterom svinger åpen. Eller det vil si, det er ikke en dør, men mer en samling perler hengene i tråder fra taket. Dette er rommet hun bruker når hun besøker Tom en gang i måneden for å passe på at han spiser litt fisk i ny og ne. Robotens luktsensor fanger lukten av ti forskjellige kubbelys som krangler om å lukte mest.",
+    steder: [
+      {
+        navn: "Kisten med kodelås",
+        beskrivelse:
+          "Du studerer kisten til Turid. Sansynligvis med hennes eiendeler som hun ikke vil at Tom eller Tommy skal bruke. Kodelåsen er en klassisk numerisk kodelås.",
+        bruk: {
+          dagenskode: {
+            beskrivelse:
+              "Du taster inn dagens kode og vipps åpner kisten. Du blir litt forbauset når alt du finner et et litt fjongt halsbånd med noe rare data-vaiere og et kretskort stikkende ut av seg. Har Turid blitt senil?",
+            gjenstand: "halsbånd",
+          },
+          turidkode: {
+            beskrivelse:
+              "Du taster inn koden, men ingenting skjer. Denne koden må være til noe annet?",
+          },
+        },
+      },
+      {
+        navn: "Bilde av Tommy og Turid",
+        beskrivelse:
+          "Turid har et bilde av henne og Tommy på Granka hengende på veggen. Du legger merke til at Turid holder en sigg i den ene hånda og en lapp med tallene '3537' i den andre. Rare greier..",
+        gjenstand: "turidkode",
+      },
+      {
+        navn: "Senga til Turid",
+        beskrivelse:
+          "En klassisk Porsgrunnsseng dette. Brun i treverket, med komplett samling av bokserien Hellefolket av Åse Gyrid Solli på nattbordet. Du finner dessverre ingenting brukanes her.",
+      },
+    ],
+  },
+  {
+    navn: "Toms rom",
+    beskrivelse:
+      "Du ruller inn døra på Toms rom. Selveste kremtoppen av Toms residens. Roboten sliter faktisk med å rulle over gulvet på grunn av alle brukermanualene for Windows XP som ligger strødd utover. Tom driver tydeligvis mye tech-support for Porsgrunn kommune herfra.",
+    steder: [
+      {
+        navn: "datamaskinen til Tom",
+        bruk: {
+          turidkode: {
+            beskrivelse:
+              "Du prøver å taste inn koden til Turid, men det skjer ingenting. Den er tydeligvis ikke ment å skulle brukes her.",
+            gjenstand: "halsbånd",
+          },
+          tommykode: {
+            beskrivelse:
+              "Du taster inn Tommys kode, og blir umiddelbart møtt av en kjent sang fra høyttalerannlegget til Tom. 'Never gonna give you up, never gonna let you down.",
+          },
+        },
+      },
+      {
+        navn: "Bilde av Tommy og Tom",
+        beskrivelse:
+          "På pulten til Tom er et bilde av han og Tommy som bader i Porsgrunnsælva. De to har tydelgivis funnet tilbake til hverandre.",
+      },
+      {
+        navn: "Gammel mat",
+        beskrivelse:
+          "På en tallerken ligger maten til Tom fra dagen før. Noen halvspiste koteletter. Du tar med en av dem.",
+        gjenstand: "koteletter",
+      },
+    ],
+  },
+  {
+    navn: "Kjellerstua",
+    beskrivelse:
+      "Du bruker de trekantformede hjulene på roboten til å rulle ned trappa til kjellerstua. Du finner etterlatt strømmeutstyr fra noe som ser ut som et forsøk på en konferanse, en gammel sofa, serveren til Toms Hosting og bikkja Horg, som sover i hjørnet.",
+    steder: [
+      {
+        navn: "Serveren til Toms Hosting",
+        bruk: {
+          turidkode: {
+            beskrivelse:
+              "Det skjer ingenting når du taster inn koden til Turid.",
+          },
+          tommykode: {
+            beskrivelse:
+              "Det skjer ingenting når du taster inn koden til Tommy.",
+          },
+          tommykode: {
+            beskrivelse:
+              "Det skjer ingenting når du setter inn minnepinnen dessverre.",
+          },
+        },
+      },
+      {
+        navn: "Bikkja Horg",
+        beskrivelse:
+          "Bikkja til Tom, Horg, sover i hjørnet av rommet, nesten i en zombie-lignende tilstand. den ser ut til å bo i kjellerstua.",
+        bruk: {
+          turidkode: {
+            beskrivelse:
+              "Det skjer ingenting når du taster inn koden til Turid.",
+          },
+          koteletter: {
+            beskrivelse:
+              "Horg reagerer ikke på kotelettene i det hele tatt, hva er galt med denne bikkja?",
+          },
+          halsbånd: {
+            beskrivelse:
+              "Du lirker halsbåndet forsiktig på Horg. Plutselig lyser det opp og begynner å lage masse forskjellige bip og bop lyder. Til din forbauselse reiser Horg seg opp på to ben, og vandrer bort mot Toms Hosting-serveren. Så tar Horg på seg lesebriller og begynner å taste infernalsk på tastaturet. Etter 20 sekunder gir han seg og faller om. Du lister deg forsiktig bort til skjermen og ser at han har skrevet gjentatte ganger 'sudo login tom passord turidruler; mv *.tomshosting *.turidshosting -R. ./start turidshosting'. Du har funnet skurken.",
+            gjenstand: "kode:turidruler",
+          },
+        },
+      },
+      {
+        navn: "Sofaen i kjellerstua",
+        beskrivelse:
+          "Du ruller bort til sofaen. En gjennomrøyka tre-seter som nok egentlig var hvit en gang i tid, men nå er mer beige-brun. Du bruker robotarmen til å kjenne mellom sofaputene og drar ut en lapp hvor det står 'Tommy 80085'. Du tar med deg lappen.",
+        gjenstand: "tommykode",
+      },
+    ],
+  },
+];
+
+var bruker = {
+  befinnerDegNå: 0,
+  gjenstander: [],
+};
+
+var errorStyle = " color: black; background: #ff0202";
+
+var printInfo = () => {
+  let regularText = "color: white; background: black;";
+  let locationStyle = "color: white; background: #0a4c0b";
+  let interactionsStyle = "color: white; background: #05717b";
+  let itemsStyle = "color: white; background: #7f0f71";
+  let commandStyle = "color: white; background: #7b4a05";
+  let lokasjon = kontoret[bruker.befinnerDegNå];
+  let steder = kontoret
+    .filter((sted) => sted.navn !== kontoret[bruker.befinnerDegNå].navn)
+    .map((sted) => '"' + sted.navn + '"')
+    .join(",");
+  if (lokasjon.steder && lokasjon.steder.length)
+    console.log(
+      `%c-- Du kan undersøke disse stedene i dette området: ${lokasjon.steder
+        .map((sted) => '"' + sted.navn + '"')
+        .join(",")}`,
+      interactionsStyle
+    );
+  else
+    console.log(
+      `%c-- Ingen steder å undersøke i dette området her. prøv å gå til et annet område.`,
+      interactionsStyle
+    );
+  if (bruker.gjenstander.length)
+    console.log(
+      `%c-- Du har disse gjenstandene du kan bruke: ${bruker.gjenstander
+        .map((gjenstand) => `"${gjenstand}"`)
+        .join(", ")}`,
+      itemsStyle
+    );
+  else console.log("%c-- Du har ingen gjenstander å bruke", itemsStyle);
+  console.log(`%c-- Du kan gå til områdene: ${steder} `, locationStyle);
+  console.log(
+    "%c-- Kommandoene tilgjengelig er gåTil(områdeNavn), undersøk(stedsNavn), brukPå(stedsNavn, gjenstand).",
+    commandStyle
+  );
+};
+
+var undersøk = (stedsNavn) => {
+  let lokasjon = kontoret[bruker.befinnerDegNå];
+  if (lokasjon.steder && lokasjon.steder.length) {
+    let locationHighlight =
+      "color: white; font-weight: bold; background: #06a8b7;  font-size: 0.8rem;";
+    let location = "color: white; background: #06a8b7;  font-size: 0.8rem;";
+    let itemHighLight =
+      "color: white; font-weight: bold; background: #e717cd;  font-size: 0.8rem;";
+    let item = "color: white; background: #e717cd;  font-size: 0.8rem;";
+    let sted = lokasjon.steder.find(
+      (sted) => sted.navn.toLowerCase() === stedsNavn.toLowerCase()
+    );
+    if (sted) {
+      console.log(
+        `%cDu undersøker ${sted.navn}: %c${sted.beskrivelse}`,
+        locationHighlight,
+        location
+      );
+      if (sted.gjenstand) {
+        bruker.gjenstander = Array.from(
+          new Set([...bruker.gjenstander, sted.gjenstand])
+        );
+        console.log(
+          `%cDu fant en gjenstand: %c${sted.gjenstand}`,
+          itemHighLight,
+          item
+        );
+      }
+    } else {
+      console.log("fant ikke stedet du lette etter på dette området.");
+    }
+  } else {
+    console.log("Dette området har ingen steder å lete.");
+  }
+  printInfo();
+};
+
+var gåTil = (områdeNavn) => {
+  let locationHighlight =
+    "color: white; font-weight: bold; background: #0084ff; font-size: 0.8rem;";
+  let location = "color: white; background: #0084ff; font-size: 0.8rem;";
+  let sted = kontoret.findIndex(
+    (sted) => områdeNavn.toLowerCase() === sted.navn.toLowerCase()
+  );
+  if (sted > -1) {
+    bruker.befinnerDegNå = sted;
+    console.log(`%c${kontoret[sted].beskrivelse}`, location);
+  } else {
+    console.log(`%cFant ikke stedet du beskrev i dette området..`, errorStyle);
+  }
+  printInfo();
+};
+
+var brukPå = (stedsNavn, gjenstand) => {
+  let locationHighlight =
+    "color: white; font-weight: bold; background: #06a8b7;  font-size: 0.8rem;";
+  let location = "color: white; background: #06a8b7;  font-size: 0.8rem;";
+  let itemHighLight =
+    "color: white; font-weight: bold; background: #e717cd;  font-size: 0.8rem;";
+  let item = "color: white; background: #e717cd;  font-size: 0.8rem;";
+
+  if (
+    bruker.gjenstander.find(
+      (gjenstand) => gjenstand.toLowerCase() === gjenstand.toLowerCase()
+    )
+  ) {
+    let lokasjon = kontoret[bruker.befinnerDegNå];
+    if (lokasjon.steder && lokasjon.steder.length) {
+      let sted = lokasjon.steder.find(
+        (sted) => sted.navn.toLowerCase() === stedsNavn.toLowerCase()
+      );
+      if (sted) {
+        if (sted.bruk && sted.bruk[gjenstand.toLowerCase()]) {
+          console.log(
+            `%c${sted.bruk[gjenstand.toLowerCase()].beskrivelse}`,
+            location
+          );
+          if (sted.bruk[gjenstand.toLowerCase()].gjenstand) {
+            bruker.gjenstander = Array.from(
+              new Set([
+                ...bruker.gjenstander,
+                sted.bruk[gjenstand.toLowerCase()].gjenstand,
+              ])
+            );
+            console.log(
+              `%cDu fant en gjenstand: %c${
+                sted.bruk[gjenstand.toLowerCase()].gjenstand
+              }`,
+              itemHighLight,
+              item
+            );
+          }
+        } else {
+          console.log(
+            `%c${gjenstand} gjør visst ingenting her gitt. Kanskje prøv en annen gjenstand?`,
+            errorStyle
+          );
+        }
+      } else {
+        console.log(
+          `%cDette stedet ${stedsNavn} finnes ikke i området du befinner deg i. Hva med å gå til et annet område?`,
+          errorStyle
+        );
+      }
+    } else {
+      console.log(
+        `%cDette området har ikke noen steder du kan bruke gjenstander på. Hva med å gå til et annet område?`,
+        errorStyle
+      );
+    }
+  } else {
+    console.log(
+      `%cDu har ikke gjenstanden ${gjenstand} i ditt inventar.`,
+      errorStyle
+    );
+  }
+  printInfo();
+};
+let welcomeHighlight =
+  "color: white; font-weight: bold; background: #8c17e7;  font-size: 1rem;";
+let welcome = "color: white; background: #8c17e7;  font-size: 1rem;";
+console.log(
+  "%c** %cTOMS %cROBOTSTYRING **",
+  welcome,
+  welcomeHighlight,
+  welcome
+);
+gåTil("Inngangen");
